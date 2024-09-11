253b3c3596484ee063da28be9974a993adffb51c
Lulliter <lmm76@georgetown.edu>
2024-09-11 18:42:23 +0200

    ___render_deploy.sh

---
+++ README.md
@@ -0,0 +1,9 @@
+# CV 
+
+CV made with quarto & `typstcv` pckg 
+
+
+# Acknowledgments  
+This follows the example of [Kazuharu Yanagimoto](https://kazuyanagimoto.com/), who is the creator of the [package](https://kazuyanagimoto.com/typstcv/) 
+fonte 
+ 
--- ___render_deploy.sh
+++ ___render_deploy.sh
@@ -1,32 +1,3 @@
-#=========================================== (Render site Locally) ================================================#
-
-# ====== Font Awesome Extension for Quarto
-quarto add quarto-ext/fontawesome
-quarto install extension shafayetShafee/bsicons # https://icons.getbootstrap.com/#icons
-quarto install extension schochastics/academicons # https://jpswalsh.github.io/academicons/
-quarto add mcanouil/quarto-iconify
-
-# ====== RENDER the entire site
-# quarto preview
-quarto preview
-
-# ====== RENDER the entire site
-quarto render
-
-# render a single file only
-project:
-  render:
-    - section1.qmd
-    - section2.qmd
-    - "*.qmd"
-    - "!ignored.qmd"
-    - "!ignored-dir/"
-
-# ====== PUBLISH
-# quarto publish  # quasto stronzo mi crea la git branch "gh-pages" -->  FIX delete branch
-			#git branch -d branch_to_delete # { not allowed IF I am on it}
-			#git branch -D branch_to_delete # { -D if you have changes that are not merged and STILL delete}
-
 #=========================================== (Push to Github repo) ================================================#
 # check status
 git status
@@ -41,16 +12,8 @@ git add tools/*
     git add   _quarto.yml
     git add     r/00_useful.qmd
 =======
-# Create Std commit "message"....
-msg="rebuilt on `date`"
-if [ $# -eq 1 ]
-  then msg="$1"
-fi
-# ... Commit Those changes.
-git commit -m "$msg"
-=======
 
-git commit -m "added Shell/*"
+git commit -m "cv.qmd"
 git commit -m "worked r/02_r_vscode.qmd 😵‍💫"
 		# git commit -m "revision INSTALL + cleanup slides 2"  -m "01_... + 00_carico_tab-contesto.qmd "
 
@@ -59,21 +22,24 @@ git commit -m "worked r/02_r_vscode.qmd 😵‍💫"
 git push origin master
 
 #=========================================== tutto insieme  ================================================#
-git add -u && git commit -a -m "added Shell/*" && git push   
+git add -u && git commit -a -m "added Shell/*" && git push
 
-#=========================================== FIle pubblico  ================================================#
-# https://quarto.org/docs/publishing/quarto-pub.html
-#  from ./
-cd .
-quarto publish quarto-pub 10_Validazione.qmd   # Published at https://lulliter.quarto.pub/validazione-dati-in-regis/
-#-->>>>>>>  (dare ENTER x farlo partire)
+#=========================================== COPIO FILE SU WEBSITE DIR  ================================================#
+#!/bin/zsh
 
-# ====== Run Script that copies things
-# PRIMA CHIUDO TUTTO WORD
-Rscript R/salvo_output_li.R
+# NB Absolute paths begin with / !!!!!!
+# Source directory (where the .pdf is located)
+SOURCE_DIR="/Users/luisamimmi/Github/cv_typst/docs"
 
-#=========================================== (IGNORE a file accidentally committed in the past) ================================================#
-# add .env file to .gitignore
-echo "accident.txt" >> .gitignore
-# tell Git NOT to track this file (it gets removed from the index, but stays local system)
-git rm --cached accident.txt
+# Destination directory (where the .pdf will be copied to)
+DEST_DIR="/Users/luisamimmi/Github/luisamimmi/cv"
+
+# File name to copy
+FILE_NAME="cv.pdf"
+
+# Copy the file
+cp "$SOURCE_DIR/$FILE_NAME" "$DEST_DIR/"
+
+# List the files in the destination directory
+echo "Files in $DEST_DIR:"
+ls "$DEST_DIR"
