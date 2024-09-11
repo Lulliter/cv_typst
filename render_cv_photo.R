3cabe278cf2d9fb45c98522b8f2741270c136662
Lulliter <lmm76@georgetown.edu>
2024-09-11 18:12:49 +0200

    cv.qmd

--- render_cv_photo.R
+++ render_cv_photo.R
@@ -1,1 +1,4 @@
-quarto::quarto_render("cv.qmd", metadata = list(`profile-photo` = "assets/img/profile.jpg"), output_file = "cv_photo.pdf")
+# This one sends 2 PDFs directly to docs/*
+quarto::quarto_render("cv.qmd",
+							 metadata = list(`profile-photo` = "assets/img/profile.jpg"),
+							 output_file = "cv_photo.pdf")
