b4c433431e729c88f09d84e313e1bea5d7d00fae
Lulliter <lmm76@georgetown.edu>
2025-03-24 16:36:41 +0100

    *_PH_eng.qmd

--- ___render_deploy.sh
+++ ___render_deploy.sh
@@ -13,7 +13,7 @@ git add -A # ALL
 git add -u # tracked
 git add images/*
 
-git commit -m "small tweaks"
+git commit -m " 2 _extensions/lulliter/* 🤯 "
 # git commit -m "revision INSTALL + cleanup slides 2"  -m "01_... + 00_carico_tab-contesto.qmd "
 git commit -m "skills into details"
 # Push local source (master branch) to remote reference (origin)
